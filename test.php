7c0288448c965fac83c25c0df2309f8d775c7e4f
nyxerebion <nyxerebion666@gmail.com>
2026-09-18 22:40:33 +0800

    Remove extra files from tracking

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'core/bootstrap.php';
-
-echo "Bootstrap loaded successfully.\n";
-echo "Hashids test: " . encodeID(24) . "\n";
-
-echo "My IP address is: " . $_SERVER['REMOTE_ADDR'] . "\n";
-
-echo decodeID('Z59'); // What does this return?
